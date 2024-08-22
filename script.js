b3cadb9ab036af8a1be9356b7201dd70c325366e
Aidan Shields <149821281+theajshields@users.noreply.github.com>
2024-08-22 15:51:16 -0700

    placeholder

--- script.js
+++ script.js
@@ -9,6 +9,6 @@ for(let i = 0; i < 1599; i++) {
 if(!window.location.search.substring(1) == "full=true") { // do not redirect if querystring is ?full=true
   if (navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i) || navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/Blackberry/i) || navigator.userAgent.match(/WebOs/i)) { // detect mobile browser
     print("Mobile Detected")
-    window.location.replace("./mobile/index.html"); // redirect if mobile browser detected
+    window.location.replace("https://rickroll.link"); // redirect if mobile browser detected
   }
 }

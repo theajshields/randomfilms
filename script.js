d284e4cf56b015a67151b01a02cc0b7ffb0ae501
Aidan Shields <149821281+theajshields@users.noreply.github.com>
2024-08-22 15:56:45 -0700

    fix my js

--- script.js
+++ script.js
@@ -6,9 +6,7 @@ for(let i = 0; i < 1599; i++) {
 }
 
 
-if(!window.location.search.substring(1) == "full=true") { // do not redirect if querystring is ?full=true
-  if ($(window).width() <= 960 && $(window).height <= 640) { /* ... */ } { // detect mobile browser
-    print("Mobile Detected")
-    window.location.replace("https://rickroll.link"); // redirect if mobile browser detected
-  }
+if ($(window).width() <= 960 && $(window).height <= 640) { /* ... */ } { // detect mobile browser
+  print("Mobile Detected")
+  window.location.replace("https://rickroll.link"); // redirect if mobile browser detected
 }
